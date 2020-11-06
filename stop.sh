d6a199cc5794dd895ef7f62d07542ed0c5f6a882
lizhifei <zhifei.li@iwellmass.com>
2020-11-06 17:27:23 +0800

    format

--- stop.sh
+++ stop.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ -f ".pid" ];then
-  pkill -9 -P `cat .pid`
-  echo "kill gitlabtools server success"
+	pkill -9 -P `cat .pid`
+	echo "kill gitlabtools server success"
 else
-  echo "gitlabtools server is not running! not need kill"
+	echo "gitlabtools server is not running! not need kill"
 fi
